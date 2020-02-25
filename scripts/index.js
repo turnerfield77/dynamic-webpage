b04812f68e7ff8df4a7f147bd33be6aef8132ba1
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-02-25 18:32:51 -0500

    Added Hello Watchkit section

--- scripts/index.js
+++ scripts/index.js
@@ -27,6 +27,10 @@ document.addEventListener('DOMContentLoaded', function(){
     container.style.paddingLeft = "10%";
     container.style.paddingRight = "10%";
 
+    /*
+        Header Section
+    */
+
     // Create elements necessary for header
     var header = document.createElement("header");
     var divTitle = document.createElement("div");
@@ -68,13 +72,17 @@ document.addEventListener('DOMContentLoaded', function(){
     home.innerHTML = "Home";
     categories.innerHTML = "Categories";
 
-
+    
     // Create new container 'main' for the rest of the page and append to container element
     var main = document.createElement("div");
     container.appendChild(main);
 
     // Style 'main'
     main.style.padding = "20px";
+
+    /*
+        Curse of the Current Reviews Section
+    */
 
     // Create new div, h1 and p tags for Reviews section and append to main
     var currentReviews = document.createElement("div");
@@ -92,5 +100,52 @@ document.addEventListener('DOMContentLoaded', function(){
     currentReviews.style.backgroundColor = "#DBD2ED";
     currentReviews.style.color = "#49464F";
     currentReviews.style.padding = "5px 20px 10px 20px";
+
+
+    /*
+        Hello Watchkit Section
+    */
+ 
+    // Create new div's for Hello WatchKit section and append to necessary parent elements
+    // 1st div to house entire section
+    // 2nd div as child to 1st to house only comments and likes
+    var helloWatchkit = document.createElement("div");
+    // var helloWatchkitFooter = document.createElement("div");
+
+    // Create child elements of the above two elements and append to necessary parent
+    var watchkitHeading = document.createElement("h2");
+    var watchkitParagraph = document.createElement("p");
+    var watchkitList = document.createElement("ul");
+    var watchkitComments = document.createElement("li");
+    var watchkitLikes = document.createElement("li");
+    main.appendChild(helloWatchkit);
+    helloWatchkit.appendChild(watchkitHeading);
+    helloWatchkit.appendChild(watchkitParagraph);
+    // helloWatchkit.appendChild(helloWatchkitFooter);
+    helloWatchkit.appendChild(watchkitList);
+    watchkitList.appendChild(watchkitComments);
+    watchkitList.appendChild(watchkitLikes);
+
+    // Add text for each necessary element
+    watchkitHeading.innerHTML = "Hello WatchKit";
+    watchkitParagraph.innerHTML = "Last month Apple released the anticipated WatchKit Framework for developers in the form of iOS 8.2 beta SDK release. The WatchKit framework enable the developers to create Apple Watch applications. In this article we are going to focus on the basics of getting started with the WatchKit framework and developing apps for the Apple Watch."
+    watchkitComments.innerHTML = "12 comments";
+    watchkitLikes.innerHTML = "124 Likes";
+
+    // Style each element
+    helloWatchkit.style.display = "flex";
+    helloWatchkit.style.flexDirection = "column";
+    helloWatchkit.style.justifyContent = "flex-start";
+    watchkitHeading.style.color = "#61BDFF";
+    watchkitHeading.style.marginBottom = "0";
+    watchkitParagraph.style.color = "#49464F";
+    watchkitParagraph.style.marginBottom = "0";
+    watchkitList.style.backgroundColor = "#F8AB11";
+    watchkitList.style.color = "white";
+    watchkitList.style.marginTop = "0";
+    watchkitList.style.padding = "5px";
+    watchkitComments.style.paddingRight = "30px";
+    watchkitComments.style.display = "inline-block";
+    watchkitLikes.style.display = "inline-block";
 
 })
